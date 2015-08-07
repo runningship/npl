3cdf008e0dfcd66e1b05457147df8dceaf96feed
runningship <runningship@gmail.com>
2015-08-07 18:05:37 +0800

    more

--- src/main/java/org/bc/logic/FactBuilder.java
+++ src/main/java/org/bc/logic/FactBuilder.java
@@ -1,25 +1,42 @@
 package org.bc.logic;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.bc.npl.entity.Fact;
 
 public class FactBuilder {
 
-	public void build(List<String> words){
+	public void build(List<String> words , List<Fact> context){
+		if(words.isEmpty()){
+			return;
+		}
+		Fact fact = new Fact();
+		List<String> subList = new ArrayList<String>();
 		for(int i=0;i<words.size();i++){
 			String word = words.get(0);
 			if("的".equals(word)){
 				if(i==0){
-					throw new RuntimeException("的字位置错误");
+					return;
 				}
 				if(i==words.size()-1){
 					//以的子结尾
-					Fact fact = new Fact();
 					fact.x = words.get(i-1);
 					fact.f = LogicConstant.F0;
+					fact.y=UUID.randomUUID().toString();
+					break;
+				}else{
+					fact.x = words.get(i-1);
+					fact.f = words.get(i+1);
+					fact.y=UUID.randomUUID().toString();
+					subList = words.subList(i+2, words.size());
+					subList.add(0,fact.y);
+					break;
 				}
 			}
 		}
+		context.add(fact);
+		build(subList , context);
 	}
 }
